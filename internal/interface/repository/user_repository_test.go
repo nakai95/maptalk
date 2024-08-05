d89df6929dde390b9b1372c28323f8bfffab36d4
koshiro1234 <gausukyanon@gmail.com>
2024-08-05 11:24:00 +0900

    repository_test作成

--- internal/interface/repository/user_repository_test.go
+++ internal/interface/repository/user_repository_test.go
@@ -1,12 +1,19 @@
 package repository
 
 import (
+	"context"
 	"maptalk/internal/domain/usecase/port"
+	"maptalk/mock"
 	"testing"
+
+	"go.uber.org/mock/gomock"
 )
 
 func TestFindById(t *testing.T) {
-	r := NewUserRepository()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ds := mock.NewMockDataStore(ctrl)
+	r := NewUserRepository(ds)
 	got, err := r.FindByID("1")
 	want := port.UserData{
 		ID:   "1",
@@ -14,5 +21,28 @@ func TestFindById(t *testing.T) {
 	}
 	if got != want || err != nil {
 		t.Errorf("FindByID() = %v, %v, want match for %v, nil", got, err, want)
+	}
+}
+
+func TestSave(t *testing.T) {
+	// when
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ds := mock.NewMockDataStore(ctrl)
+	r := NewUserRepository(ds)
+	user := port.UserData{
+		ID:   "1",
+		Name: "John Doe",
+	}
+	ds.EXPECT().InsertData(gomock.Any(), gomock.Any())
+
+	context := context.Background()
+	got, err := r.Save(user, context)
+	want := port.UserData{
+		ID:   "1",
+		Name: "John Doe",
+	}
+	if got != want || err != nil {
+		t.Errorf("Save() = %v, %v, want match for %v, nil", got, err, want)
 	}
 }
